d22b6cee8b613966028f1f5f3985a136129c54b0
Alec Clowes <alec@laurel.ai>
2025-07-15 05:41:18 -0700

    first pass chatbot

---
+++ chatbot/README.md
@@ -0,0 +1,10 @@
+# chatbot
+
+```shell
+python3 -m venv venv
+. ./venv/bin/activate
+pip install -r requirements.txt
+
+export OPENAI_API_KEY=xxx
+python chatbot.py
+```
---
+++ chatbot/chatbot.py
@@ -0,0 +1,47 @@
+# TODO:
+#   Tell it we have beer and sake, but don't talk about it unless they ask
+#   Include URL?
+#   Structured response with URL, price, photo URL, description why.
+
+import os
+
+import gradio
+
+from openai import OpenAI
+from openai.types.responses.response_prompt_param import ResponsePromptParam
+from openai.types.responses.response_input_param import EasyInputMessageParam
+
+client = OpenAI(
+    api_key=os.environ['OPENAI_API_KEY']
+)
+
+
+# TODO set cookies https://nickyreinert.medium.com/how-to-achieve-persistence-in-a-gradio-app-c3612084b5e2
+
+def predict(message, history):
+    print(f"User input: {message}")
+    response = client.responses.create(
+        model="gpt-4.1",
+        prompt=ResponsePromptParam(id="pmpt_68571d5f865881938d46b7a7defc77c100c288bd4c2fc620"),
+        input=[EasyInputMessageParam(content=row['content'], role=row['role']) for row in history]
+              + [EasyInputMessageParam(content=message, role='user')],
+        # TODO utilize previous_response_id and stream
+    )
+
+    print(f"Consumed {response.usage.total_tokens} tokens")
+    return response.output_text
+
+
+demo = gradio.ChatInterface(
+    predict,
+    type="messages",
+    examples=["Recommend a Sardinian red, please!", "What kind of sake and beer do you have?",
+              "Is there a creamy white from Italy?"],
+    css="footer {display:none !important}",
+    save_history=True,
+    cache_examples=True,
+    cache_mode="lazy",
+    title="Welcome to Bevi Bene's Sommelier Chatbot"
+)
+
+demo.launch()
